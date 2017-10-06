5ea055343078f53a6e44c58b5f239e5dc89eb3a5
Swizec Teller <swizec@swizec.com>
2017-10-06 13:33:08 -0700

    Basic fractal tree

--- src/DancingTree.js
+++ src/DancingTree.js
@@ -1,9 +1,74 @@
 
 import React, { Component } from 'react';
 
-const Pythagoras = ({ width, height, x, y }) => (
-    <rect width={width} height={height} x={x} y={y} />
-);
+Math.deg = function(radians) {
+  return radians * 180 / Math.PI;
+};
+
+const memoizedCalc = function () {
+    const memo = {};
+
+    const key = ({ w, heightFactor, lean }) => [w,heightFactor, lean].join('-');
+
+    return (args) => {
+        const memoKey = key(args);
+
+        if (memo[memoKey]) {
+            return memo[memoKey];
+        }else{
+            const { w, heightFactor, lean } = args;
+
+            const trigH = heightFactor*w;
+
+            const result = {
+                nextRight: Math.sqrt(trigH**2 + (w * (.5+lean))**2),
+                nextLeft: Math.sqrt(trigH**2 + (w * (.5-lean))**2),
+                A: Math.deg(Math.atan(trigH / ((.5-lean) * w))),
+                B: Math.deg(Math.atan(trigH / ((.5+lean) * w)))
+            };
+
+            memo[memoKey] = result;
+            return result;
+        }
+    }
+}();
+
+const Pythagoras = ({ width, height, lvl, maxlvl, x, y, left, right }) => {
+    if (lvl >= maxlvl) {
+        return null;
+    }
+
+    const { nextRight, nextLeft, A, B } = memoizedCalc({
+        w: width,
+        heightFactor: .4,
+        lean: 0
+    });
+
+    let rotate = '';
+
+    if (left) {
+        rotate = `rotate(${-A} 0 ${width})`;
+    }else if (right) {
+        rotate = `rotate(${B} ${width} ${width})`;
+    }
+
+    return (
+        <g transform={`translate(${x} ${y}) ${rotate}`}>
+            <rect width={width} height={width} x={0} y={0} />
+
+            <Pythagoras width={nextLeft}
+                        height={height}
+                        x={0} y={-nextLeft}
+                        lvl={lvl+1} maxlvl={maxlvl}
+                        left />
+            <Pythagoras width={nextRight}
+                        height={height}
+                        x={width-nextRight} y={-nextRight}
+                        lvl={lvl+1} maxlvl={maxlvl}
+                        right />
+        </g>
+    )
+};
 
 class DancingTree extends Component {
     render() {
@@ -12,7 +77,10 @@ class DancingTree extends Component {
         return (
             <svg width={width} height={height}>
                 <Pythagoras width={100} height={100}
-                            x={width/2-25} y={height-100} />
+                            x={width/2-25} y={height-100}
+                            lvl={0}
+                            maxlvl={10}
+                            lean={0} />
             </svg>
         );
     }
