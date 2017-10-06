5ffa3e1280ec961f76deeb8605ddb1d93b79e5de
Swizec Teller <swizec@swizec.com>
2017-10-06 16:54:07 -0700

    Basic routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import logo from './logo.svg';
 import owl from './random-image-005.jpg';
 import './App.css';
 
+import { Route } from 'react-router';
+import { BrowserRouter, Link } from 'react-router-dom';
+
 import Header from './Header';
 import DancingTree from './DancingTree';
 import MortysMindblowers from './MortysMindblowers';
 
+const TwoTrees = () => (
+    <div>
+        <DancingTree width={600} height={200} />
+        <DancingTree width={600} height={200} />
+    </div>
+);
+
 class App extends Component {
   render() {
-    return [
-        //<DancingTree width={640} height="480" />
-        <MortysMindblowers />
-    ];
+      return <BrowserRouter>
+    <div>
+        <nav>
+            <Link to="/morty">Morty's Mindblowers</Link>
+            &nbsp;|&nbsp;
+            <Link to="/tree">Dancing Tree</Link>
+        </nav>
+        <Route path="/morty" component={MortysMindblowers} />
+        <Route path="/tree" component={TwoTrees} />
+    </div>
+      </BrowserRouter>
   }
 }
 
