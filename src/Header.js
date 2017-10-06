2b4c38155e7994cd4e222e66c54de3e6f7214937
Swizec Teller <swizec@swizec.com>
2017-10-06 13:10:04 -0700

    Missed files

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const Header = (props) => (
+const Header = ({ props }) => (
     <header className="App-header">
         <img src={props.logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
